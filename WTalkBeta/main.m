a856c9bb2756345f4c32a1062de0d5601794650f
silicon <silicon@silicontekiMac-mini.local>
2014-09-24 14:34:28 +0800

    Initial Commit

---
+++ WTalkBeta/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  WTalkBeta
+//
+//  Created by silicon on 14-9-24.
+//  Copyright (c) 2014年 silicon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ WTalkBetaTests/WTalkBetaTests.m
@@ -0,0 +1,34 @@
+//
+//  WTalkBetaTests.m
+//  WTalkBetaTests
+//
+//  Created by silicon on 14-9-24.
+//  Copyright (c) 2014年 silicon. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface WTalkBetaTests : XCTestCase
+
+@end
+
+@implementation WTalkBetaTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
